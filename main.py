8f0f6d0a4687fb3484eed661f8ca2f737d71f5be
Kenneth Yang <kjy@is88.com>
2026-06-08 23:13:23 -0700

    Configured default build for 10um

--- main.py
+++ main.py
@@ -4,20 +4,19 @@ from brainglobe_atlasapi import BrainGlobeAtlas
 from zarr import create_array
 from zarr.codecs import BloscCodec, BloscShuffle
 
-ATLAS_NAME = "allen_mouse_50um"
+ATLAS_NAME = "allen_mouse_10um"
 
 
 def main():
-    # noinspection PyTypeChecker
     atlas = BrainGlobeAtlas(ATLAS_NAME, check_latest=True)
     chunk_width = ceil(sqrt(1_000_000 / 4 / atlas.shape[1]))
     z = create_array(
-        store=f"out/{ATLAS_NAME}.zarr",
+        store=f"out/{ATLAS_NAME}/annotation.zarr",
         shape=atlas.shape,
         chunks=(chunk_width, atlas.shape[1], chunk_width),
         shards=(chunk_width * 3, atlas.shape[1], chunk_width * 3),
         dtype=atlas.annotation.dtype,
-        compressors=BloscCodec(cname="zstd", clevel=3, shuffle=BloscShuffle.bitshuffle),
+        compressors=BloscCodec(shuffle=BloscShuffle.bitshuffle),
         overwrite=True,
     )
     z[:] = atlas.annotation
