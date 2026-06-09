620f709a4d0b6ce5871b20f1cde94d28e36d8353
Kenneth Yang <kjy@is88.com>
2026-06-09 00:10:49 -0700

    Group atlas zarr under same atlas name

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 from math import ceil, sqrt
+from pprint import pprint
 
 from brainglobe_atlasapi import BrainGlobeAtlas
 from zarr import create_array
 from zarr.codecs import BloscCodec, BloscShuffle
 
-ATLAS_NAME = "allen_mouse_10um"
-
 
 def main():
-    atlas = BrainGlobeAtlas(ATLAS_NAME, check_latest=True)
+    atlas = BrainGlobeAtlas("allen_mouse_10um", check_latest=True)
     chunk_width = ceil(sqrt(1_000_000 / 4 / atlas.shape[1]))
     z = create_array(
-        store=f"out/{ATLAS_NAME}/annotation.zarr",
+        store=f"out/{atlas.metadata['name']}/{atlas.metadata['resolution'][0]}.zarr",
         shape=atlas.shape,
         chunks=(chunk_width, atlas.shape[1], chunk_width),
         shards=(chunk_width * 3, atlas.shape[1], chunk_width * 3),
