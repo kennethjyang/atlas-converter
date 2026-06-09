09b953d7e6b7a37116e7fafead2a8b649bb8b167
Kenneth Yang <kjy@is88.com>
2026-06-08 23:42:53 -0700

    Compress with zarr demo (#1)
    * Download and compress allen 50um
    
    * Configured default build for 10um
    
    * Added analysis workflows
    
    * Use default token

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
+from math import ceil, sqrt
+
+from brainglobe_atlasapi import BrainGlobeAtlas
+from zarr import create_array
+from zarr.codecs import BloscCodec, BloscShuffle
+
+ATLAS_NAME = "allen_mouse_10um"
+
+
 def main():
-    print("Hello from atlas-converter!")
+    atlas = BrainGlobeAtlas(ATLAS_NAME, check_latest=True)
+    chunk_width = ceil(sqrt(1_000_000 / 4 / atlas.shape[1]))
+    z = create_array(
+        store=f"out/{ATLAS_NAME}/annotation.zarr",
+        shape=atlas.shape,
+        chunks=(chunk_width, atlas.shape[1], chunk_width),
+        shards=(chunk_width * 3, atlas.shape[1], chunk_width * 3),
+        dtype=atlas.annotation.dtype,
+        compressors=BloscCodec(shuffle=BloscShuffle.bitshuffle),
+        overwrite=True,
+    )
+    z[:] = atlas.annotation
+    print(z.info_complete())
 
 
 if __name__ == "__main__":
