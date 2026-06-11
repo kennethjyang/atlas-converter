ba15857014e9a267ca428ec8340191c0d8e3e5ec
Kenneth Yang <kjy@is88.com>
2026-06-10 18:45:32 -0700

    Re-enable compression

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from zarr.codecs import BloscCodec, BloscShuffle
 
 
 def main():
-    atlas = BrainGlobeAtlas("allen_mouse_10um", check_latest=True)
+    atlas = BrainGlobeAtlas("allen_mouse_100um", check_latest=True)
     print("Loaded atlas")
 
     # Remap structure ID's to consecutive identifiers from 1 onward.
@@ -18,20 +18,19 @@ def main():
     mapped_index = searchsorted(ids, atlas.annotation)
     print("Create mapping from old to new")
     remapped_annotation = new_ids[mapped_index]
-    pprint(remapped_annotation)
 
-    # chunk_width = ceil(sqrt(1_000_000 / 4 / atlas.shape[1]))
-    # z = create_array(
-    #     store=f"out/{atlas.metadata['name']}/{atlas.metadata['resolution'][0]}.zarr",
-    #     shape=atlas.shape,
-    #     chunks=(chunk_width, atlas.shape[1], chunk_width),
-    #     shards=(chunk_width * 3, atlas.shape[1], chunk_width * 3),
-    #     dtype=atlas.annotation.dtype,
-    #     compressors=BloscCodec(shuffle=BloscShuffle.bitshuffle),
-    #     overwrite=True,
-    # )
-    # z[:] = atlas.annotation
-    # print(z.info_complete())
+    # Compress with Zarr.
+    chunk_width = ceil(sqrt(1_000_000 / 4 / atlas.shape[1]))
+    z = create_array(
+        store=f"out/{atlas.metadata['name']}/{atlas.metadata['resolution'][0]}.zarr",
+        shape=atlas.shape,
+        chunks=(chunk_width, atlas.shape[1], chunk_width),
+        shards=(chunk_width * 3, atlas.shape[1], chunk_width * 3),
+        dtype=atlas.annotation.dtype,
+        compressors=BloscCodec(shuffle=BloscShuffle.bitshuffle),
+        overwrite=True,
+    )
+    z[:] = remapped_annotation
 
 
 if __name__ == "__main__":
