e6dd08518e96e8bf241a1230b914d02ccea89534
Kenneth Yang <kjy@is88.com>
2026-06-25 22:44:10 -0700

    Fixed function semantic naming

--- annotation_compressor.py
+++ annotation_compressor.py
@@ -2,10 +2,10 @@ from brainglobe_atlasapi import BrainGlobeAtlas
 from numpy import dtype, ndarray, uint16
 from pandas import Categorical
 
-from atlas_manager import get_sorted_structure_ids
+from atlas_manager import sorted_structure_ids
 
 
-def remap_annotation_ids(
+def remapped_annotation_ids(
     atlas: BrainGlobeAtlas,
 ) -> ndarray[tuple[int, int, int], dtype[uint16]]:
     """Returns remap annotation values to the sorted structure IDs order.
@@ -15,6 +15,6 @@ def remap_annotation_ids(
     """
     flat_atlas = atlas.annotation.ravel()
     remapped_flat = Categorical(
-        flat_atlas, categories=get_sorted_structure_ids(atlas)
+        flat_atlas, categories=sorted_structure_ids(atlas)
     ).codes.astype(uint16)
     return remapped_flat.reshape(atlas.shape)
